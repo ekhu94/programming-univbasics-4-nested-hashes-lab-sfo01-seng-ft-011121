5877df1dee168a6d82439e86ee596eaec70b9d92
Erik Huang <erikhuangdrums@gmail.com>
2020-12-17 00:20:18 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -114,6 +114,8 @@ def changing_alan
       }
     }
     
+    programmer_hash[:alan_kay][:known_for] = "GUI"
+    programmer_hash
 
 end
 
@@ -137,7 +139,6 @@ def adding_to_dennis
       }
     }
     
-    programmer_hash[:alan_kay][:known_for] = "GUI"
-    programmer_hash
+    
 
 end
